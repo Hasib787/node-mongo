81a6c07847da21cbe8b5691467e2529a70f76933
Hasib787 <mr.hasib45@gmail.com>
2021-03-26 02:36:19 +0600

    use postman to api test

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 const users =["Sakib", "Shohan", "Riad", "Rakin", "Nurullah"]
 app.get('/', (req, res) =>{
@@ -22,6 +27,12 @@ app.get('/users/:id', (req, res) =>{
     const id = req.params.id;
     const name= users[id];
     res.send({id,name});
+})
+
+
+//post
+app.post('/addUser', (req, res) =>{
+    console.log(req.body);
 })
 
 app.listen(3000, ()=> console.log('Listening to port 3000'));
