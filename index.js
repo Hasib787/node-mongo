0ed4a979b377b7606dd11db2bc171461536a5a69
Hasib787 <mr.hasib45@gmail.com>
2021-03-25 23:59:01 +0600

    dynamic api use parameter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+const users =["Sakib", "Shohan", "Riad", "Rakin", "Nurullah"]
 app.get('/', (req, res) =>{
     const fruit ={
         product: 'laptop',
@@ -16,4 +17,9 @@ app.get('/fruit/banana', (req, res) =>{
     res.send({fruit:'banana', quantity:200, price: 2000})
 })
 
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id;
+    const name= users[id];
+    res.send(id,name);
+})
 app.listen(3000, ()=> console.log('Listening to port 3000'));
