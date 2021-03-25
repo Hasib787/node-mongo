c3b678fcb2d7bf75b1d951fa4396e388c80e6ec9
Hasib787 <mr.hasib45@gmail.com>
2021-03-26 00:11:07 +0600

    added some changes

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ app.get('/fruit/banana', (req, res) =>{
     res.send({fruit:'banana', quantity:200, price: 2000})
 })
 
+
 app.get('/users/:id', (req, res) =>{
     const id = req.params.id;
     const name= users[id];
-    res.send(id,name);
+    res.send({id,name});
 })
+
 app.listen(3000, ()=> console.log('Listening to port 3000'));
