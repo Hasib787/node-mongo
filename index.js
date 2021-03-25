e32d9ff45e36c303a2a374bbd0c20e44598ece1a
Hasib787 <mr.hasib45@gmail.com>
2021-03-25 21:19:51 +0600

    make node surver

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send('Thank you for calling me');
+    const fruit ={
+        product: 'laptop',
+        price: 550
+    }
+    res.send(fruit);
+})
+
+app.get('/fruit/banana', (req, res) =>{
+    res.send({fruit:'banana', quantity:200, price: 2000})
 })
 
 app.listen(3000, ()=> console.log('Listening to port 3000'));
